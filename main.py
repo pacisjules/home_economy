17a23b50230fb310bd98821ec8c3639912e09271
Ishimwe Jules Pacis (Mediator) <pacisjules@gmail.com>
2022-01-27 19:17:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ async def home():
     }
 
 
+@app.get("/check/{item}", tags=["Home"])
+async def home(item: str):
+    return {
+        "Check Item" : item
+    }
+
 @app.get("/app/info", tags=["App"])
 async def app_info():
     return {
