fd396f162a329fe701e9f3c384ed1ef82a741b13
Ishimwe Jules Pacis (Mediator) <pacisjules@gmail.com>
2022-01-27 17:34:08 +0200

    Update connection.py

--- configs/connection.py
+++ configs/connection.py
@@ -9,8 +9,8 @@ import os
 import re
 
 uri = os.getenv("postgres://lrcbqxortfgumm:4ef5aae613c7e2caaf6bc39ae950e9d670e401a4d6af5a866c5fa5ee5abaf7f6@ec2-34-233-157-189.compute-1.amazonaws.com:5432/d655pfbfgl0o78")  # or other relevant config var
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
+if uri.startswith("postgres://lrcbqxortfgumm:4ef5aae613c7e2caaf6bc39ae950e9d670e401a4d6af5a866c5fa5ee5abaf7f6@ec2-34-233-157-189.compute-1.amazonaws.com:5432/d655pfbfgl0o78"):
+    uri = uri.replace("postgres://lrcbqxortfgumm:4ef5aae613c7e2caaf6bc39ae950e9d670e401a4d6af5a866c5fa5ee5abaf7f6@ec2-34-233-157-189.compute-1.amazonaws.com:5432/d655pfbfgl0o78", "postgresql://lrcbqxortfgumm:4ef5aae613c7e2caaf6bc39ae950e9d670e401a4d6af5a866c5fa5ee5abaf7f6@ec2-34-233-157-189.compute-1.amazonaws.com:5432/d655pfbfgl0o78", 1)
 # rest of connection code using the connection string `uri`
 
 
