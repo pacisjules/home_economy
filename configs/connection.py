863d2d34a2beb70ae002193763a30ad0cf0a7f98
Ishimwe Jules Pacis (Mediator) <pacisjules@gmail.com>
2022-01-27 17:21:19 +0200

    Update connection.py

--- configs/connection.py
+++ configs/connection.py
@@ -16,7 +16,7 @@ def DATABASE_URL(
     port: str       = "5432",
     database: str   = "d655pfbfgl0o78"
 ):
-    return str(connection+"://"+username+":"+password+"@"+host+":"+port+"/"+database)
+    return str("postgres://lrcbqxortfgumm:4ef5aae613c7e2caaf6bc39ae950e9d670e401a4d6af5a866c5fa5ee5abaf7f6@ec2-34-233-157-189.compute-1.amazonaws.com:5432/d655pfbfgl0o78")
 
 database = databases.Database(DATABASE_URL())
 
