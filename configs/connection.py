629fcae44e75ff41da419b133991741415073720
Ishimwe Jules Pacis (Mediator) <pacisjules@gmail.com>
2022-01-27 17:12:25 +0200

    credential
    credential

--- configs/connection.py
+++ configs/connection.py
@@ -9,12 +9,12 @@ def db_config():
     return dbinfo.Setting()
 
 def DATABASE_URL(
-    connection: str = "postgresql",
-    username: str   = "pacis",
-    password: str   = "123",
-    host: str       = "127.0.0.1",
+    connection: str = "postgres://lrcbqxortfgumm:4ef5aae613c7e2caaf6bc39ae950e9d670e401a4d6af5a866c5fa5ee5abaf7f6@ec2-34-233-157-189.compute-1.amazonaws.com:5432/d655pfbfgl0o78",
+    username: str   = "lrcbqxortfgumm",
+    password: str   = "4ef5aae613c7e2caaf6bc39ae950e9d670e401a4d6af5a866c5fa5ee5abaf7f6",
+    host: str       = "ec2-34-233-157-189.compute-1.amazonaws.com",
     port: str       = "5432",
-    database: str   = "Home_economy"
+    database: str   = "d655pfbfgl0o78"
 ):
     return str(connection+"://"+username+":"+password+"@"+host+":"+port+"/"+database)
 
